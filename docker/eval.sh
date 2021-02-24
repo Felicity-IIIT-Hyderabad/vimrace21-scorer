e7b4af43ba16bb8e8362ed6bf6f5ea0814c44d73
yoogottamk <yoogottamk@outlook.com>
2021-02-24 22:32:33 +0530

    run vim as non root

--- docker/eval.sh
+++ docker/eval.sh
@@ -6,11 +6,21 @@ err=0
 corr=0
 
 for f in *.in; do
-    q="$( echo $f | cut -d. -f1 )"
+    q="$( echo "$f" | cut -d. -f1 )"
     echo "Test $q"
 
-    vim -u NONE -Z -n +0 -s solve "$f" >/dev/null
+    # change permissions for input file
+    chown vimgolf:vimgolf "$f"
 
+    # run vim as vimgolf user so they cant edit any other files
+    su - vimgolf -c "vim -u NONE -Z -n +0 -s solve $f >/dev/null" &
+    pid=$!
+
+    # kill process after 3s
+    sleep 3
+    kill $pid
+
+    # check if output/input are different
     if diff -a "${q}.out" "${q}.in"; then
         echo "Passed"
         corr="$( echo "${corr}+1" | bc )"
